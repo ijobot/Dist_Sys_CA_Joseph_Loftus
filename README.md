2b800425e94b96ce123ef8c75ceb7e763a4d7ec9
Joe Loftus <joe@ijobot.tech>
2025-04-16 01:12:16 +0100

    major rewrites to all protos, functions written for lightService, README added.

---
+++ README.md
@@ -0,0 +1,21 @@
+# J&J Brick Shippers Smart Offices Control System
+
+This system controls the climate, lighting, and security for the offices of J&J Brick Shippers.
+
+## Climate
+
+The climate control system gets updated temperature and humidity readings every 30 minutes and then sets the internal climate controls to keep the office environment cool and comfortable.  It is an automated system that is triggered at 6am every morning and shuts off at 7pm.
+
+## Lighting
+
+The light controls are user initiated and can be accessed by any employee that is granted permission.  Individual lights can be set, as well as all lights in a given room.  Both brightness and color can be changed for each bulb.
+
+## Security
+
+The security system is accessible only by authorized security personel.  As employees enter the building, they are asked for their EmployeeNumber (which is given by swiping their ID card or tapping their ID fob) and subsequently asked to enter a 4-digit code that changes monthly.
+
+## Discovery
+
+The discovery service handles all other services and manages the overall system.
+
+## This file will be updated regularly as the system changes.
--- server/lightService.js
+++ server/lightService.js
@@ -7,8 +7,20 @@ const definition = protoLoader.loadSync(LIGHT_PROTO_PATH);
 const lightProto = grpc.loadPackageDefinition(definition).light;
 
 const lights = [
-  { id: 1, name: "Light 1", color: "#222222" },
-  { id: 2, name: "Light 2", color: "#777777" },
+  { id: 1, room: "conference", brightness: 100, color: "bright white" },
+  { id: 2, room: "conference", brightness: 100, color: "bright white" },
+  { id: 3, room: "conference", brightness: 100, color: "bright white" },
+  { id: 4, room: "office 1", brightness: 70, color: "light blue" },
+  { id: 5, room: "office 1", brightness: 70, color: "light blue" },
+  { id: 6, room: "office 2", brightness: 0, color: "warm yellow" },
+  { id: 7, room: "office 2", brightness: 0, color: "warm yellow" },
+  { id: 8, room: "entrance", brightness: 100, color: "red" },
+  { id: 9, room: "entrance", brightness: 100, color: "green" },
+  { id: 10, room: "entrance", brightness: 100, color: "red" },
+  { id: 11, room: "entrance", brightness: 100, color: "green" },
+  { id: 12, room: "hallway", brightness: 90, color: "soft white" },
+  { id: 13, room: "hallway", brightness: 90, color: "soft white" },
+  { id: 14, room: "elevator", brightness: 60, color: "warm yellow" },
 ];
 
 const getLight = (call, callback) => {
@@ -23,9 +35,56 @@ const getLight = (call, callback) => {
   }
 };
 
+const getRoomLights = (call, callback) => {
+  const roomlights = lights.filter((light) => light.room !== call.request.room);
+  if (roomlights) {
+    callback(null, roomlights);
+  } else {
+    callback({
+      code: grpc.status.NOT_FOUND,
+      details: "Room not found",
+    });
+  }
+};
+
+const setLight = (call, callback) => {
+  const light = lights.find((light) => light.id === call.request.id);
+  if (light) {
+    light.brightness = call.request.brightness;
+    light.color = call.request.color;
+    const confirmationMessage = `Light ${light.id} has been set to brightness: ${light.brightness} and color: ${light.color}.`;
+    callback(null, { confirmationMessage });
+  } else {
+    callback({
+      code: grpc.status.NOT_FOUND,
+      details: "There was a problem.  Light has not been set.",
+    });
+  }
+};
+
+const setRoomLights = (call, callback) => {
+  const roomlights = lights.filter((light) => light.room !== call.request.room);
+  if (roomlights) {
+    roomlights.forEach((rm) => {
+      (rm.brightness = call.request.brightness),
+        (rm.color = call.request.color);
+    });
+    const confirmationMessage = `All lights in room ${call.request.room} have been set to brightness: ${light.brightness} and color: ${light.color}.`;
+    callback(null, confirmationMessage);
+  } else {
+    callback({
+      code: grpc.status.NOT_FOUND,
+      details: "Room not found",
+    });
+  }
+};
+
 const server = new grpc.Server();
 server.addService(lightProto.LightService.service, {
   GetLight: getLight,
+  GetRoomLights: getRoomLights,
+  SetLight: setLight,
+  SetRoomLights: setRoomLights,
 });
 server.bindAsync(
   "127.0.0.1:50052",
